a3438cb3fc414a27e2b1cb416e3218e32ec963be
pdereg <pawel@deregowski.email>
2017-02-04 14:49:10 +0100

    Added initial Gradle and Docker configurations, along with bare-bone Spring applications for each service

---
+++ api-gateway/src/main/java/com/pdereg/timelogger/Application.java
@@ -0,0 +1,15 @@
+package com.pdereg.timelogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring application for the API Gateway.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ log-service/src/main/java/com/pdereg/timelogger/Application.java
@@ -0,0 +1,15 @@
+package com.pdereg.timelogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring application for the Log Service.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
